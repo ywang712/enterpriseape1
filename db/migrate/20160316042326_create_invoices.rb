157a5e12bf1a91a496ef87e01d15cc479cd14a51
Yong Wang <ywang712@gmail.com>
2016-03-16 16:21:38 +0000

    first commit

---
+++ db/migrate/20160316042326_create_invoices.rb
@@ -0,0 +1,12 @@
+rake db:migrateCrails class CreateInvoices < ActiveRecord::Migration
+  def change
+    create_table :invoices do |t|
+      t.datetime :date
+      t.string :company
+      t.decimal :tax
+      t.string :salesperson
+
+      t.timestamps null: false
+    end
+  end
+end
